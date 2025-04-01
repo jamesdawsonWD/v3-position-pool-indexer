db98ed32112ed26687df197dfed4cfaa66fdf5b2
James Dawson <jamesdawsonWD@gmail.com>
2025-04-01 12:45:42 +0100

    update: remove position manager

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -4,7 +4,7 @@ import './handlers/collect';
 import './handlers/burn';
 import './handlers/mint';
 import './handlers/swap';
-import './handlers/transfer'
-import './handlers/positionCollect'
-import './handlers/increaseLiquidity';
-import './handlers/decreaseLiquidity';
+// import './handlers/transfer'
+// import './handlers/positionCollect'
+// import './handlers/increaseLiquidity';
+// import './handlers/decreaseLiquidity';
